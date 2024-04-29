36656705bdd6f2be1eef81fb53ab2b545c5dd9a9
adriano.rodrigues <adriano.rodrigues@g.globo>
2024-04-29 16:02:26 -0300

    feat: create filter for solo/duo/trio/five

--- src/extract.js
+++ src/extract.js
@@ -4,9 +4,30 @@
     playerTarget: '.sc-7bda52f2-1.jrvggu',
     historyContainer: '.sc-17dce764-1.dKRkDg > div > div',
     matchClassName: '',
+    dateAndResultContainer: '.sc-20c903f9-3.bdnait',
   };
 
-  execute();
+  const state = {
+    filters: [],
+  };
+
+  chrome.runtime.onMessage.addListener((message) => {
+    if (message === undefined) return;
+
+    const { action, value } = message;
+
+    if (action === 'EXECUTE') {
+      execute();
+
+      return;
+    }
+
+    if (action === 'FILTER_UPDATE') {
+      state.filters = value;
+
+      return;
+    }
+  });
 
   // ACTIONS
   function execute() {
@@ -15,20 +36,45 @@
     const matches = getValidMatches(historyContainer);
 
     expandAllMatches(matches);
-
-    console.log(matches);
+    const matchData = getMatchData(matches);
+    dataToHTML(matchData);
   };
 
   function getValidMatches(container) {
     const { childNodes: matches } = container;
 
-    return Array.from(matches).filter((match) => match.className === IDs.matchClassName);
+    return (
+      Array
+        .from(matches)
+        .filter((match) => match.className === IDs.matchClassName)
+        .map((match) => match.querySelector('div'))
+    );
   }
 
   function expandAllMatches(matches) {
     matches.forEach((match) => {
       match.click();
     });
+  }
+
+  function getMatchData(matches) {
+    return matches
+      .map((match) => {
+        const dateAndResult = match.querySelectorAll(IDs.dateAndResultContainer);
+        const filterValue = (dateAndResult[0].childNodes[2] || dateAndResult[0].childNodes[1]).textContent.split(' ')[0].toLowerCase();
+
+        if (!state.filters.includes(filterValue)) return undefined;
+
+        const result = dateAndResult[0].childNodes[0].textContent;
+        const date = dateAndResult[1].childNodes[0].textContent;
+        const dataContainer = match.nextSibling;
+
+        return {
+          date,
+          result,
+        };
+      })
+      .filter((match) => match !== undefined);
   }
 
   function dataToHTML(data) {
@@ -39,7 +85,62 @@
           <title>Unite Extractor</title>
         </head>
         <body>
-          <table><tr><td>${data}</td></tr></table>
+          <table>
+            <tr>
+              <td colspan="12">Team Data</td>
+              <td colspan="10">Opponents Data</td>
+            </tr>
+            <tr>
+              <td>Date</td>
+              <td>Result</td>
+              <td>Player</td>
+              <td>Pokemon</td>
+              <td>Points</td>
+              <td>Kills</td>
+              <td>Assists</td>
+              <td>Damage Given</td>
+              <td>Damage Received</td>
+              <td>Heal</td>
+              <td>Draft priority</td>
+              <td>Bans</td>
+              <td>Bans</td>
+              <td>Draft priority</td>
+              <td>Heal</td>
+              <td>Damage Received</td>
+              <td>Damage Given</td>
+              <td>Assists</td>
+              <td>Kills</td>
+              <td>Points</td>
+              <td>Pokemon</td>
+              <td>Player</td>
+            </tr>
+            ${data.map((item) => (
+              `<tr>
+                <td>${item.date}</td>
+                <td>${item.result}</td>
+                <td>${item.player}</td>
+                <td>${item.pokemon}</td>
+                <td>${item.points}</td>
+                <td>${item.kills}</td>
+                <td>${item.assists}</td>
+                <td>${item.dmgGiven}</td>
+                <td>${item.dmgReceived}</td>
+                <td>${item.heal}</td>
+                <td>${item.draftPriority}</td>
+                <td>${item.bans}</td>
+                <td>${item.opponnetBans}</td>
+                <td>${item.opponentDraftPriority}</td>
+                <td>${item.opponentHeal}</td>
+                <td>${item.opponentDmgReceived}</td>
+                <td>${item.opponentDmgGiven}</td>
+                <td>${item.opponentAssists}</td>
+                <td>${item.opponentKills}</td>
+                <td>${item.opponentPoints}</td>
+                <td>${item.opponentPokemon}</td>
+                <td>${item.opponentPlayer}</td>
+              </tr>`
+            ))}
+          </table>
         </body>
       </html>
     `;
--- src/main.js
+++ src/main.js
@@ -3,28 +3,34 @@
   const elements = {
     buttonExtract: '#button-extract',
     errorContent: '#error-content',
-    content: '#content',
+    filterItems: '#filter-container > input'
   };
 
-  init();
-
-  // ACTIONS
-  function init() {
+  document.addEventListener('DOMContentLoaded', async () => {
     try {
+      const currentTab = await getCurrentTab();
       const buttonExtract = document.querySelector(elements.buttonExtract);
 
       buttonExtract.addEventListener('click', extract);
+      chrome.scripting.executeScript({ target: { tabId: currentTab.id }, files: ['./src/extract.js'] });
     } catch (e) {
       showError(e);
     }
-  }
+  });
 
+  // ACTIONS
   function showError(error) {
     const errorContent = document.querySelector(elements.errorContent);
     const errorItem = document.createElement('div');
 
     errorItem.innerText = error.message;
     errorContent.appendChild(errorItem);
+  }
+
+  function getFilters() {
+    const filterElements = document.querySelectorAll(elements.filterItems);
+
+    return Array.from(filterElements).filter((item) => item.checked).map((item) => item.value);
   }
 
   async function getCurrentTab() {
@@ -38,7 +44,8 @@
     try {
       const currentTab = await getCurrentTab();
 
-      chrome.scripting.executeScript({ target: { tabId: currentTab.id }, files: ['./src/extract.js'] });
+      chrome.tabs.sendMessage(currentTab.id, { action: 'FILTER_UPDATE', value: getFilters() });
+      chrome.tabs.sendMessage(currentTab.id, { action: 'EXECUTE' });
     } catch (e) {
       showError(e);
     }
